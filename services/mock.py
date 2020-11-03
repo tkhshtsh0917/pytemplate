c3be9399893a3a2d67a391ff404fe3976e870a68
Toshinori Takahashi <48384135+tkhshtsh0917@users.noreply.github.com>
2020-11-04 02:10:30 +0900

    [Add] Add workflow for integration testing

--- services/mock.py
+++ services/mock.py
@@ -10,18 +10,28 @@ from starlette.status import HTTP_200_OK
 api = FastAPI()
 
 
-@api.get("/api/mock", status_code=HTTP_200_OK)
-async def service():
+@api.get("/api/mock/{pattern}", status_code=HTTP_200_OK)
+async def service(pattern: str) -> dict:
     """
     service
 
+    Args:
+        pattern (str): Execution type. "ptn01" or "ptn02" or "ptn03".
+
     Returns:
-        dict: Response message.
+        dict: Response data (JSON).
     """
+
     response = {"status": HTTP_200_OK, "contents": []}
 
     contents = []
-    filepathlist = glob.glob("data/*.json")
+    if pattern == "ptn01":
+        filepathlist = glob.glob("data/sample01.json")
+    elif pattern == "ptn02":
+        filepathlist = glob.glob("data/sample02.json")
+    elif pattern == "ptn03":
+        filepathlist = glob.glob("data/sample03.json")
+
     for filepath in filepathlist:
         with open(filepath, "r") as file:
             data = json.load(file)
