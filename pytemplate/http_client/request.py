c3fae8c8e943854636b1e7d902208614b76cff4e
Toshinori Takahashi <48384135+tkhshtsh0917@users.noreply.github.com>
2020-11-04 02:10:30 +0900

    [Add] Add workflow for integration testing

--- pytemplate/http_client/request.py
+++ pytemplate/http_client/request.py
@@ -2,21 +2,53 @@
 request
 """
 
+import sys
 import requests
 from starlette.status import HTTP_200_OK
+
+
+def parser(response, output_name):
+    """
+    parser
+
+    Args:
+        response (Response): Returned HTTP request.
+        output_name (str): Output file name.
+    """
+    if response.status_code == HTTP_200_OK:
+        data = response.json()
+
+        if data["status"] == HTTP_200_OK:
+            with open(output_name, mode="w", encoding="utf-8") as file:
+                for msg in data["contents"]:
+                    print(msg["text"])
+                    file.write(msg["text"])
+                    file.write("\n")
 
 
 def request():
     """
     request
     """
-    response = requests.get("http://localhost:8000/api/mock")
-    if response.status_code == HTTP_200_OK:
-        data = response.json()
 
-        if data["status"] == HTTP_200_OK:
-            with open("output.txt", mode="w", encoding="utf-8") as file:
-                for msg in data["contents"]:
-                    print(msg["text"])
-                    file.write(msg["text"])
-                    file.write("\n")
+    mode = ""
+    if len(sys.argv) > 1:
+        mode = sys.argv[1]
+
+    if mode == "CI":
+        URL = "http://localhost:8000/api/mock/"
+
+        # Server 01
+        response = requests.get(URL + "ptn01")
+        parser(response=response, output_name="output01.txt")
+
+        # Server 02
+        response = requests.get(URL + "ptn02")
+        parser(response=response, output_name="output02.txt")
+
+        # Server 03
+        response = requests.get(URL + "ptn03")
+        parser(response=response, output_name="output03.txt")
+
+    else:
+        print("Hello, world")
