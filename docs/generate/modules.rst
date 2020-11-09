0ab6c09fe01b8ade2b13ff59c043bdb49d495f3b
tkhshtsh0917 <48384135+tkhshtsh0917@users.noreply.github.com>
2020-11-10 02:55:18 +0900

    [#-][Add] Add pre-commit configulation

--- docs/generate/modules.rst
+++
@@ -1,9 +0,0 @@
-pytemplate
-==========
-
-.. toctree::
-   :maxdepth: 4
-
-   memory_analyzer
-   pytemplate
-   tests
--- setup.sh
+++ setup.sh
@@ -6,20 +6,8 @@ if [ $COMMIT_TEMPLATE != '.github\\COMMIT_TEMPLATE\\.commit_template' ]; then
   echo "Updated commit message template."
 fi
 
-PRECOMMIT_MASTER_FILE='./.github/PRE_COMMIT_MASTER/pre-commit'
-if [ -e $PRECOMMIT_MASTER_FILE ]; then
-  diff -s $PRECOMMIT_MASTER_FILE ./.git/hooks/pre-commit > /dev/null 2>&1
-  if [ $? -ne 0 ]; then
-    cp $PRECOMMIT_MASTER_FILE ./.git/hooks/pre-commit
-    echo "Updated pre-commit."
-    echo "Pre-commit was not latest. Please run again." 1>&2
-    exit 1
-  fi
-else
-  echo "$PRECOMMIT_MASTER_FILE doesn't exist."
-fi
-
 poetry install
+poetry run pre-commit install
 if [ $? -eq 0 ]; then
   echo "Project setup is complete."
 fi
